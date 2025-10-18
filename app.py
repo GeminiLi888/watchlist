c2dd1d95ec923076d6858c3d729d98111b11888a
Goat Li <goatli@li.cn>
2025-10-18 22:59:14 +0800

    Add forms to create,edit and delete items

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import sys
-from tkinter.tix import Select
-
+from flask import request, url_for, redirect, flash
 import click
 from sqlalchemy import select
 from flask import Flask, render_template
@@ -14,6 +13,7 @@ SQLITE_PREFIX = 'sqlite:///' if sys.platform.startswith('win') else 'sqlite:////
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLITE_PREFIX + str(Path(app.root_path) / 'data.db')
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 class Base(DeclarativeBase):
     pass
 db = SQLAlchemy(app, model_class=Base)
@@ -67,15 +67,47 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 # def hello():
 #     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title').strip()
+        year = request.form.get('year').strip()
+        if not title or not year or len(title) > 60 or len(year) > 4:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie=Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
     user=db.session.execute(select(User)).scalar()
     movies=db.session.execute(select(Movie)).scalars().all()
     return render_template('index.html', user=user, movies=movies)
-from sqlalchemy import select
-
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie=db.get_or_404(Movie,movie_id)
+    if request.method == 'POST':
+        title=request.form.get('title').strip()
+        year=request.form.get('year').strip()
+        if not title or not year or len(title) > 60 or len(year)!=4:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+        movie.title=title
+        movie.year=year
+        db.session.commit()
+        flash('Item Updated.')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = db.get_or_404(Movie, movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
 @app.context_processor
 def inject_user():  # 函数名可以随意修改
     user = db.session.execute(select(User)).scalar()
@@ -85,7 +117,6 @@ def inject_user():  # 函数名可以随意修改
 def page_not_found(error):
     #user=db.session.execute(select(User)).scalar()
     return render_template('404.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
 
